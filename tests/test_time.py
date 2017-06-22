e7bbbd5fdba8eaf84e3da9f0ef9920387b54e77a
jakubtustanowski <tusty53@gmail.com>
2017-06-22 17:05:47 +0200

    Time tests ok

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,19 +1,19 @@
 from freezegun import freeze_time
-import datetime
-from twitter import in_previous_24h
+from datetime import datetime
+from ..twitter import in_previous_24h
 
-@freeze_time("2012-01-14 03:21:34")
+
+@freeze_time("2015-06-19 12:25:37")
 def test_now():
-    assert in_previous_24h("2012-01-14 03:21:34")
+    assert in_previous_24h("Fri Jun 19 12:25:37 +0000 2015")
 
-@freeze_time("2012-01-14 03:21:34")
+
+@freeze_time("2015-06-19 12:25:37")
 def test_hour_ago():
-    assert in_previous_24h("2012-01-14 02:21:34")
+    assert in_previous_24h("Fri Jun 19 11:25:37 +0000 2015")
 
-@freeze_time("2012-01-14 03:21:34")
+
+@freeze_time("2015-06-19 12:25:37")
 def test_yesterday():
-    assert not in_previous_24h("2012-01-14 03:21:34")
+    assert not in_previous_24h("Thu Jun 18 10:25:36 +0000 2015")
 
-@freeze_time("2012-01-14 03:21:34")
-def test_now_timestamp():
-    assert in_previous_24h(datetime.now().timestamp())
